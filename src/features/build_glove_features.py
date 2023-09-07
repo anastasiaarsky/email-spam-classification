f53e16bc065eda89ab07824b3ab8a4af068a2821
anastasiaarsky <anarsky@gmail.com>
2023-09-07 16:52:35 -0700

    edit build_glove_features.py to save tokenizer used in models/tokenizer.pickle

--- src/features/build_glove_features.py
+++ src/features/build_glove_features.py
@@ -1,6 +1,7 @@
 import zipfile
 import numpy as np
 import pandas as pd
+import pickle
 
 from sklearn.model_selection import train_test_split
 from keras.layers import Embedding
@@ -20,6 +21,10 @@ def vectorize_data(X_train, X_test, max_len):
     # Fit the tokenizer onto the training data
     tokenizer = Tokenizer(num_words=vocab_size, oov_token="")
     tokenizer.fit_on_texts(X_train)
+
+    # Save the tokenizer
+    with open('models/tokenizer.pickle', 'wb') as handle:
+        pickle.dump(tokenizer, handle, protocol=pickle.HIGHEST_PROTOCOL)
 
     # Convert the training and testing sets to sequences
     X_train_seq = tokenizer.texts_to_sequences(X_train)
